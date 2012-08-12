6eb09f36026a606947e97e2a40acd5903e737258
Eugene Serkin <jeserkin@gmail.com>
2012-08-12 14:22:11 +0300

    Some editing

--- tests/Lertify/Lastfm/ClientTest.php
+++ tests/Lertify/Lastfm/ClientTest.php
@@ -8,32 +8,20 @@ namespace Lertify\Lastfm\Tests;
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
 	/**
-     * @var \Lertify\Lastfm\Client
-     */
-    protected $lastfm;
+	 * @var \Lertify\Lastfm\Client
+	 */
+	protected $lastfm;
+
+	protected function setUp()
+	{
+		$this->lastfm = new \Lertify\Lastfm\Client( $GLOBALS['api_key'] );
+	}
 
 	/**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
-    protected function setUp()
-    {
-        $this->lastfm = new \Lertify\Lastfm\Client( $GLOBALS['api_key'] );
-    }
-
-	/**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown()
-    {
-    }
-
-	/**
-     * @covers {className}::{origMethodName}
-     */
-    public function testGetConfiguration()
-    {
-        throw new \Exception( 'TODO Implement Me' );
-    }
+	 * @throws \Exception
+	 */
+	public function testGetConfiguration()
+	{
+		throw new \Exception( 'TODO Implement Me' );
+	}
 }
